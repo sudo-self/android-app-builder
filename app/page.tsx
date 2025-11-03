e4e1e2d569f2ced286e4160aed88084ee9d454fe
sudo-self <sudo-self@proton.me>
2025-11-03 04:29:20 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -817,12 +817,12 @@ export default function APKBuilder() {
                         className={`text-xs text-center ${isDarkMode ? "text-slate-400" : "text-slate-600"}`}
                       >
                         <a
-                          href="https://apk.jessejesse.com/workflow.png"
+                          href="https://github.com/sudo-self/apk-builder-actions"
                           target="_blank"
                           rel="noopener noreferrer"
                           className="underline hover:text-pink-500"
                         >
-                          github actions yml
+                          Apk Builder Source Code
                         </a>
                       </p>
 
